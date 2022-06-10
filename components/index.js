8e001facd151e2575a449f6b28fdf9e8ea232351
Britishexchangestudent <44755600+Britishexchangestudent@users.noreply.github.com>
2022-06-10 12:44:04 +0100

    Banner

--- components/index.js
+++ components/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-cycle */
 export { default as Navbar } from './Navbar';
 export { default as Footer } from './Footer';
 export { default as Button } from './Button';
+export { default as Banner } from './Banner';
